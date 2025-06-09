9243bd01de869d747da7911bb48c946e7929bc94
Ashutosh Rai <127360478+AshutoshRai15@users.noreply.github.com>
2025-06-09 21:17:59 +1000

    Add files via upload

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import './Navbar.css'
-import nav_logo from '../../assets/website_logo.png'
-import navProfile from '../../assets/nav-profile.svg'
+import React, { useContext, useState } from 'react';
+import './Navbar.css';
+
+
+import logo from '../Assets/logo.png'
+import cart from '../Assets/cart.png'
+import { Link } from 'react-router-dom';
+import { ShopContext } from '../../Context/ShopContext';
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        <img src={nav_logo} alt="" className="nav-logo" />
-        <img src={navProfile} className='nav-profile' alt="" />
-    </div>
-  )
+
+        const [menu,setMenu] = useState("shop");
+        const {getTotalCartItems}= useContext(ShopContext)
+
+    return (
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p>Braeside Smart Mart</p>
+            </div>
+            <ul className='nav-menu'>
+                <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Home</Link>{menu==="shop"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("accessories")}}><Link style={{ textDecoration: 'none' }} to='/accessories'>Accessories</Link>{menu==="accessories"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("stationery")}}><Link style={{ textDecoration: 'none' }} to='/stationery'>Stationery</Link>{menu==="stationery"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("uniform")}}><Link style={{ textDecoration: 'none' }} to='/uniform'>Uniform</Link>{menu==="uniform"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("cadet corps")}}><Link style={{ textDecoration: 'none' }} to='/cadet corps'>Cadet Corps</Link>{menu==="cadet corps"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("gifts")}}><Link style={{ textDecoration: 'none' }} to='/gifts'>Gifts</Link>{menu==="gifts"?<hr/>:<></>}</li>
+            </ul>
+            <div className="nav-logo-cart">
+                {localStorage.getItem('auth-token')
+                ?<button onClick={()=>{localStorage.removeItem('auth-token');window.location.replace('/')}}>Logout</button>
+                :<Link to='/login'><button>Login</button></Link>}
+                <Link to='/cart'><img src={cart} alt="" /></Link>
+                <div className="nav-cart-count">{getTotalCartItems()}</div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
