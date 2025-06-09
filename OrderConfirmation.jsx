d46aba9e7c53bd58e94a0c5fc05122f73b9907a5
Ashutosh Rai <127360478+AshutoshRai15@users.noreply.github.com>
2025-06-09 20:58:29 +1000

    Add files via upload

---
+++ OrderConfirmation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Confirmation from '../Components/Confirmation/Confirmation'
+
+const OrderConfirmation = () => {
+  return (
+    <div>
+        <Confirmation/>
+    </div>
+  )
+}
+
+export default OrderConfirmation
