0d26184a639703bdc601c3a7ea64485ab8c440b7
Ashutosh Rai <127360478+AshutoshRai15@users.noreply.github.com>
2025-06-09 21:11:02 +1000

    Add files via upload

---
+++ DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Introducing Braeside Smart Mart, the premier e-commerce destination for The King’s School community. Our user-friendly platform is designed to provide a seamless shopping experience, offering a wide range of products from school supplies to exclusive merchandise. With easy account creation using your school credentials, Braeside Smart Mart ensures secure and personalized access for students, parents, and staff.</p>
+            <p>Join the Braeside Smart Mart community today and enjoy the convenience of 24/7 access to all your favorite products, tailored specifically for The King’s School family. Embrace the future of shopping with Braeside Smart Mart where convenience meets quality, and your needs are always our priority.</p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
